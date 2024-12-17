b799b7d18d774bc19dd41916b28b823be414b3a0
Tyler <tylerlarson919@gmail.com>
2024-12-16 19:19:40 -0500

    minor tweaks

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,8 @@ const nextConfig: NextConfig = {
           {
             key: 'Content-Security-Policy',
             value: "default-src 'self'; " +
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com https://apis.google.com; " +
-              "script-src-elem 'self' 'unsafe-inline' https://www.youtube.com https://s.ytimg.com https://apis.google.com; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com https://apis.google.com; https://vercel.live; " +
+              "script-src-elem 'self' 'unsafe-inline' https://www.youtube.com https://s.ytimg.com https://apis.google.com; https://vercel.live; " +
               "style-src 'self' 'unsafe-inline'; " +
               "frame-src 'self' https://example.com https://www.youtube.com https://podo-fe249.firebaseapp.com; " +
               "connect-src 'self' https://firebase.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://www.googleapis.com https://www.youtube.com https://firestore.googleapis.com; " + // Added Firestore URL
